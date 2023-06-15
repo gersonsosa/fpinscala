12b3ba190b248a4f30b982552548cbf362c616f4
Gerson Sosa <4366859+gersonsosa@users.noreply.github.com>
2023-06-15 05:03:35 +0000

    ex: chapter 4 Either

--- src/main/scala/fpinscala/exercises/errorhandling/Either.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Either.scala
@@ -4,30 +4,55 @@ package fpinscala.exercises.errorhandling
 import scala.{Either as _, Left as _, Right as _}
 import scala.util.control.NonFatal
 
-enum Either[+E,+A]:
+enum Either[+E, +A]:
   case Left(get: E)
   case Right(get: A)
 
-  def map[B](f: A => B): Either[E, B] = ???
+  def map[B](f: A => B): Either[E, B] = this match
+    case Left(e)  => Left(e)
+    case Right(a) => Right(f(a))
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def getOrElse[E, B >: A](default: => B): B = this match
+    case Left(get)  => default
+    case Right(get) => get
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match
+    case Left(get)  => Left(get)
+    case Right(get) => f(get)
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+    map(a => Right(a)).getOrElse(b)
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {
+      flatA <- this
+      flatB <- b
+    } yield f(flatA, flatB)
 
 object Either:
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  def traverse[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es.foldRight[Either[E, List[B]]](Right(Nil))((h, t) => f(h).map2(t)(_ :: _))
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+  def traverse0[E, A, B](
+      as: List[A]
+  )(f: A => Either[E, B]): Either[E, List[B]] =
+    as match
+      case head :: next => f(head).map2(traverse0(next)(f))(_ :: _)
+      case Nil          => Right(Nil)
 
-  def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
-    if xs.isEmpty then
-      Left("mean of empty list!")
-    else 
-      Right(xs.sum / xs.length)
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    traverse(es)(a => a)
 
-  def safeDiv(x: Int, y: Int): Either[Throwable, Int] = 
+  def sequence0[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    es match
+      case head :: next => head.map2(sequence0(next))(_ :: _)
+      case Nil          => Right(Nil)
+
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
+    if xs.isEmpty then Left("mean of empty list!")
+    else Right(xs.sum / xs.length)
+
+  def safeDiv(x: Int, y: Int): Either[Throwable, Int] =
     try Right(x / y)
     catch case NonFatal(t) => Left(t)
 
@@ -35,8 +60,24 @@ object Either:
     try Right(a)
     catch case NonFatal(t) => Left(t)
 
-  def map2All[E, A, B, C](a: Either[List[E], A], b: Either[List[E], B], f: (A, B) => C): Either[List[E], C] = ???
+  def map2All[E, A, B, C](
+      a: Either[List[E], A],
+      b: Either[List[E], B],
+      f: (A, B) => C
+  ): Either[List[E], C] =
+    for {
+      first <- a
+      second <- b
+    } yield f(first, second)
 
-  def traverseAll[E, A, B](as: List[A], f: A => Either[List[E], B]): Either[List[E], List[B]] = ???
+  def traverseAll[E, A, B](
+      as: List[A],
+      f: A => Either[List[E], B]
+  ): Either[List[E], List[B]] =
+    as.foldRight(Right(Nil): Either[List[E], List[B]])((e, acc) =>
+      map2All(f(e), acc, _ :: _)
+    )
 
-  def sequenceAll[E, A](as: List[Either[List[E], A]]): Either[List[E], List[A]] = ???
+  def sequenceAll[E, A](
+      as: List[Either[List[E], A]]
+  ): Either[List[E], List[A]] = traverseAll(as, identity)
--- src/main/scala/fpinscala/exercises/errorhandling/errorhandling.worksheet.sc
+++ src/main/scala/fpinscala/exercises/errorhandling/errorhandling.worksheet.sc
@@ -1,3 +1,5 @@
+import scala.Option as _
+import fpinscala.exercises.errorhandling.Option
 import fpinscala.exercises.errorhandling.Option.*
 
 val test = Some(2)
@@ -11,7 +13,28 @@ n.orElse(Some(5))
 
 test.filter { t => t == 1 }
 
-variance(List(1,2,3,4,5))
+variance(List(1, 2, 3, 4, 5))
 
 sequence(List(Some(1), Some(2), Some(3)))
 traverse(List(1, 2, 3))(a => Some(a.toDouble))
+
+// Hide std library `Either` since we are writing our own in this chapter
+import scala.{Either as _, Left as _, Right as _}
+import fpinscala.exercises.errorhandling.Either
+import fpinscala.exercises.errorhandling.Either.*
+
+val e: Either[String, Int] = Right(123)
+e.map(n => n * 10)
+e.flatMap {
+  case n: Int if n > 10 => Right(true)
+  case _: Int           => Left("Not enough")
+}
+
+val el: Either[String, String] = Left("The other side")
+el.getOrElse("Actual side")
+
+el.orElse(Right("The good side"))
+
+val b = Right(20)
+
+e.map2(b)(_ + _)
