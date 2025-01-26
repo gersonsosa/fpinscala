bbba44111ec12a3d376cb1b3a697b8c8e06cf323
Gerson Sosa <4366859+gersonsosa@users.noreply.github.com>
2025-01-26 12:51:54 +0100

    ex: 10.17

--- src/main/scala/fpinscala/exercises/monoids/Monoid.scala
+++ src/main/scala/fpinscala/exercises/monoids/Monoid.scala
@@ -161,8 +161,8 @@ object Monoid:
     val empty = (ma.empty, mb.empty)
 
   given functionMonoid[A, B](using mb: Monoid[B]): Monoid[A => B] with
-    def combine(f: A => B, g: A => B) = ???
-    val empty: A => B = a => ???
+    def combine(f: A => B, g: A => B) = (a) => mb.combine(f(a), g(a))
+    val empty: A => B = a => mb.empty
 
   given mapMergeMonoid[K, V](using mv: Monoid[V]): Monoid[Map[K, V]] with
     def combine(a: Map[K, V], b: Map[K, V]) = ???
