45aa6b09a77b4280c00b6ee89fa5f7cf269902ab
Gerson Sosa <4366859+gersonsosa@users.noreply.github.com>
2025-02-08 17:44:28 +0100

    ex: 11.1 - 11.6
    Implementing sequence and traverse was hard, the only way to access the
    inner A or B is to use map2 to concatenate to an existing list.
    
    The replicateM for list result is surprising, but the rest just produces
    a list of the inner repeated element.

--- src/main/scala/fpinscala/exercises/monads/Monad.scala
+++ src/main/scala/fpinscala/exercises/monads/Monad.scala
@@ -30,7 +30,7 @@ trait Monad[F[_]] extends Functor[F]:
   extension [A](fa: F[A])
     def flatMap[B](f: A => F[B]): F[B] =
       fa.map(f).join
-    
+
     def map[B](f: A => B): F[B] =
       fa.flatMap(a => unit(f(a)))
 
@@ -38,13 +38,16 @@ trait Monad[F[_]] extends Functor[F]:
       fa.flatMap(a => fb.map(b => f(a, b)))
 
   def sequence[A](fas: List[F[A]]): F[List[A]] =
-    ???
+    fas.foldRight(unit(List.empty[A]))((fa, acc) => fa.map2(acc)(_ :: _))
 
   def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
-    ???
+    as.foldRight(unit(List.empty[B]))((a, acc) => f(a).map2(acc)(_ :: _))
 
+  // NOTE: will create a list of F which can be a List[List[A]]
+  // this will invoke flatMap which creates a new List/LazyList
+  // and fills it with the given a (which will be references to the original)
   def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
-    ???
+    fa.map(a => List.fill(n)(a))
 
   def compose[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] =
     ???
@@ -53,8 +56,11 @@ trait Monad[F[_]] extends Functor[F]:
     def flatMapViaCompose[B](f: A => F[B]): F[B] =
       ???
 
+  // NOTE: builds a Monad of elements that satisfies the function a -> F[Boolean]
+  // Option of list with elements that satisfies f
+  // List of list with elements that satisfies f
   def filterM[A](as: List[A])(f: A => F[Boolean]): F[List[A]] =
-    ???
+    as.foldRight(unit(List.empty[A]))((a, acc) => f(a).flatMap(bool => if bool then unit(a).map2(acc)(_ :: _) else acc))
 
   extension [A](ffa: F[F[A]]) def join: F[A] =
     ???
@@ -66,7 +72,7 @@ trait Monad[F[_]] extends Functor[F]:
   def composeViaJoinAndMap[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] =
     ???
 
-end Monad      
+end Monad
 
 object Monad:
   given genMonad: Monad[Gen] with
@@ -76,34 +82,42 @@ object Monad:
         Gen.flatMap(fa)(f)
 
   given parMonad: Monad[Par] with
-    def unit[A](a: => A) = ???
+    def unit[A](a: => A) = Par.unit(a)
     extension [A](fa: Par[A])
       override def flatMap[B](f: A => Par[B]): Par[B] =
-        ???
+        fa.chooser(f)
 
   def parserMonad[P[+_]](p: Parsers[P]): Monad[P] = new:
-    def unit[A](a: => A) = ???
+    def unit[A](a: => A) = p.succeed(a)
     extension [A](fa: P[A])
       override def flatMap[B](f: A => P[B]): P[B] =
-        ???
+        p.flatMap(fa)(f) // the flatMap implementation is defined in a extension that takes a parser
 
   given optionMonad: Monad[Option] with
-    def unit[A](a: => A) = ???
+    def unit[A](a: => A) = Some(a)
     extension [A](fa: Option[A])
       override def flatMap[B](f: A => Option[B]) =
-        ???
+        fa.flatMap(f) // the implementation inlines
 
   given lazyListMonad: Monad[LazyList] with
-    def unit[A](a: => A) = ???
+    def unit[A](a: => A) = LazyList(a)
     extension [A](fa: LazyList[A])
       override def flatMap[B](f: A => LazyList[B]) =
-        ???
+        fa.flatMap(f) // the implementation of LazyList.flatMap is not trivial
 
   given listMonad: Monad[List] with
-    def unit[A](a: => A) = ???
+    def unit[A](a: => A) = List(a)
     extension [A](fa: List[A])
       override def flatMap[B](f: A => List[B]) =
-        ???
+        fa.flatMap(f) // internally uses releaseFence so no trivial
+
+  // NOTE: define the type S over the whole monad was this too simple?
+  given stateMonad[S]: Monad[State[S, _]] with
+    def unit[A](a: => A) = State.unit(a)
+    extension [A](fa: State[S, A])
+      override def flatMap[B](f: A => State[S, B]) =
+        // flatMap is defined in an extension that takes an underlying state
+        State.flatMap(fa)(f)
 
 end Monad
 
